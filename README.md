e9fd9f007194257151a279a1e4a96bd8481491e1
Ariqusu77 <100405305+Ariqusu77@users.noreply.github.com>
2024-01-13 15:04:24 +0700

    ver 1.1

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # SuaraKita
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.10.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SelectCardBaseComponent } from './select-card-base/select-card-base.component';
+import { ClarityModule } from "@clr/angular";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SelectCardBaseComponent],
+  imports: [ CommonModule, RouterOutlet, SelectCardBaseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
